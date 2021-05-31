ff9a58612a82dc8abe179f6e6555a378447ab7c3
2108Sharan <20011066@myrp.edu.sg>
2021-05-31 20:18:31 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20011066/demooptionmenutranslation/MainActivity.java
@@ -0,0 +1,48 @@
+package sg.edu.rp.c346.id20011066.demooptionmenutranslation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvTranslatedText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvTranslatedText = findViewById(R.id.textViewTranslatedText);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.EnglishSelection) {
+            tvTranslatedText.setText("Hello");
+            return true;
+        }
+        else if (id == R.id.italianSelection) {
+            tvTranslatedText.setText("Ciao");
+            return true;
+        }
+        else {
+            tvTranslatedText.setText("Error translation");
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
